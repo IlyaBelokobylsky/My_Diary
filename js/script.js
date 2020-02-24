4e7136e31807b168469055757b80325ec07a6bf3
IlyaBelokobylsky <ilya.belokobylskiy.leopard@gmail.com>
2020-02-24 23:04:39 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -2,53 +2,38 @@
 const menu = document.querySelector('.hamburger');
 const header = document.querySelector('header');
 const container = document.querySelector('.container');
-let scrollHeight = Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight
-);
 
 function headerToggle() {
-    container.classList.toggle('container-translated')
     menu.classList.toggle('menu-clicked');
-    header.classList.toggle('header-visible');
     document.querySelector('.btn-up').classList.toggle('btn-up-hidden');
+    container.classList.toggle('container-translated')
 
-    if (header.classList.contains('header-visible')) {
+    if (menu.classList.contains('menu-clicked')) {
+        // Без этого перемещение наверх резкое
+        scrollToElem(document.querySelector('.head-screen'));
+
+        // Убрать/вернуть скролл
         document.documentElement.style.overflow = 'hidden';
-        if (scrollHeight >= 993) {
-            scrollToElem('head-screen');
-        }
+        pageYOffset > 0 ?
+        setTimeout(() => document.documentElement.style.position = 'fixed', 1500) :
+        document.documentElement.style.position = 'fixed';
     } else {
-        document.documentElement.style.overflow = '';
+        document.documentElement.style.position = 'absolute';
+        document.documentElement.style.overflow = 'visible';
     }
 }
 
-function scrollToElem(id) {
-    let elemTo = document.querySelector('.' + id);
-    elemTo.classList.contains('make-and-settings') ?// для более корректной работы 
-    elemTo.scrollIntoView({ behavior: 'smooth', block: 'start' }) :
-    elemTo.scrollIntoView({ behavior: 'smooth', block: 'end' });
+function scrollToElem(elem) {
+    elem.classList.contains('make-and-settings') ?// для более корректной работы 
+    elem.scrollIntoView({ behavior: 'smooth', block: 'start' }) :
+    elem.scrollIntoView({ behavior: 'smooth', block: 'end' });
 }
 
 menu.addEventListener('click', headerToggle);
-
-header.addEventListener('click', function (event) {
-    const target = event.target;
-
-
-});
-
-
-
-
-
-
 
 // Анимация для блюра на первом экране
 for (let i = 3; i >= 1; i--) {
     setTimeout(() => 
         document.querySelector(`.head-screen__phone-blur-${i}`).classList.add(`head-screen__phones_blur-visible-${i}`)
-    , 1900 * Math.pow(i, -1));
+    , 1333 * Math.pow(i, -1));
 }
-
