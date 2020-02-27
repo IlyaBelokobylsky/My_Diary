a728a9eadceb798734315d51649b1acd8059e7a7
IlyaBelokobylsky <ilya.belokobylskiy.leopard@gmail.com>
2020-02-27 18:24:39 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,23 +3,34 @@ const menu = document.querySelector('.hamburger'),
     header = document.querySelector('header'),
     container = document.querySelector('.container'),
     navMenu = document.querySelector('.header__nav_list'),
-    arrow = document.querySelector('.btn-up');
+    arrow = document.querySelector('.btn-up'),
+    sections = document.querySelectorAll('.js-section');
 
 
 function headerToggle() {
+    let headerActiveLink = document.querySelector(`.nav__link[href*=head-screen]`).parentNode;
+    
+    
+
     menu.classList.toggle('menu-clicked');
     document.querySelector('.btn-up').classList.toggle('btn-up-hidden');
-    container.classList.toggle('container-translated')
+    container.classList.toggle('container-translated');
+    
+
     
     if (menu.classList.contains('menu-clicked')) {
         // Без этого перемещение наверх резкое
-        document.querySelector('.head-screen').scrollIntoView({ behavior: 'smooth', block: 'nearest' })
-        
+        document.body.scrollIntoView({ behavior: 'smooth', block: 'start' })
+
         // Убрать/вернуть скролл
         document.documentElement.style.overflow = 'hidden';
-        pageYOffset > 0 ?
-        setTimeout(() => document.documentElement.style.position = 'fixed', 1200) :
-        document.documentElement.style.position = 'fixed';
+
+        // С задержкой или нет?
+        setTimeout(() => {
+            document.documentElement.style.position = 'fixed';
+            document.querySelector('.nav__active-link').classList.remove('nav__active-link')
+            headerActiveLink.classList.add('nav__active-link');
+        }, 1300) 
     } else {
         document.documentElement.style.position = 'absolute';
         document.documentElement.style.overflow = 'visible';
@@ -28,21 +39,36 @@ function headerToggle() {
 
 menu.addEventListener('click', headerToggle);
 
+// Изменение активной ссылки в хедере при скролле
+document.addEventListener('scroll', function (event) {
+    const centerX = document.documentElement.clientWidth / 2,
+        centerY = document.documentElement.clientHeight / 2,
+        centerElem = document.elementFromPoint(centerX, centerY),
+        centerSection = centerElem.closest('.js-section');
+    if (!centerSection) return;
+    let headerActiveLink = document.querySelector(`.nav__link[href*=${centerSection.id}]`).parentNode;
+    let headerBeforeActiveLink = document.querySelector('.nav__active-link');
+    headerBeforeActiveLink.classList.remove('nav__active-link');
+    headerActiveLink.classList.add('nav__active-link')
+});
+
 
 // Обработка нажатий на ссылки в nav меню
 navMenu.addEventListener('click', function (event) {
-    event.preventDefault(); // дабы не было "как обычно"
-    const href = event.target.getAttribute('href');
-    const beforeActive = document.querySelector('.nav__active-link');
-    const active = event.target.closest('li');
+    if (event.target.closest('a')) {
+        event.preventDefault(); // дабы не было "как обычно"
+        const href = event.target.getAttribute('href');
+        const beforeActive = document.querySelector('.nav__active-link');
+        const active = event.target.closest('li');
 
-    beforeActive.classList.remove('nav__active-link');
-    active.classList.add('nav__active-link');
+        beforeActive.classList.remove('nav__active-link');
+        active.classList.add('nav__active-link');
 
-    headerToggle();
-    setTimeout(function() { // чтобы хедер успел убраться
-        document.querySelector(`${href}`).scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-    }, 600);
+        headerToggle();
+        setTimeout(function() { // чтобы хедер успел убраться
+            document.querySelector(`${href}`).scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+        }, 600);
+    }
 });
 
 
@@ -61,3 +87,19 @@ document.addEventListener('scroll', function(event) {
     pagePosition = pageYOffset;
 })
 arrow.onclick = () => container.scrollIntoView({ behavior: 'smooth', block: 'start'});
+
+
+// Появление телефона при наведении на текст
+document.querySelectorAll('.why-we__list_elem-js').forEach(function(item) {
+    item.onmouseover = () => {
+        let phone = document.querySelector('.why-we__phone');
+        phone.classList.remove('hidden');
+        phone.classList.add('visible');
+    };
+    
+    item.onmouseout = () => { 
+        let phone = document.querySelector('.why-we__phone');
+        phone.classList.remove('visible');
+        phone.classList.add('hidden');
+    };
+});
